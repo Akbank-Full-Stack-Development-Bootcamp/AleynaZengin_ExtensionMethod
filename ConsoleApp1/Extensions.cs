75a1d3a54710959e3bfa1cd37ebb464c22cc8ae8
aleynazengin <aleystyles23@gmail.com>
2021-06-06 19:38:51 +0300

    Add project files.

---
+++ ConsoleApp1/Extensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Extensions
+    {
+        
+
+        public static double Round(this double value, int digits)
+        {
+            return Math.Round(value, digits);
+        }
+
+       
+        public static int [] addNumberstoArray(this int value)
+        {
+            int[] array = new int[value];
+            for (int i = 0; i < value; i++)
+            {
+                Console.Write("Number {0} : ", i+1);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return array;
+        }
+
+        public static int findMaxNumberinArray(this int[] array)
+        {
+
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+
+        }
+        public static int findMinNumberinArray(this int[] array)
+        {
+
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Opening_Message();
+            
+        }
+        public static void Opening_Message()
+        {
+
+            Console.WriteLine("\n1- Round number");
+            Console.WriteLine("2- Find max and min number in array");
+            Console.WriteLine("Enter the number for the programme you want to do. To exit press 0");
+            int choice = int.Parse(Console.ReadLine());
+            Menuhelper(choice);
+        }
+
+        public static void Menuhelper(int value)
+        {
+            Console.Clear();
+            switch (value)
+            {
+                case 1:
+                    roundProgramme();
+                    break;
+                case 2:
+                    ArrayProgramme();
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+
+            }
+            Opening_Message();
+
+        }
+
+        private static void ArrayProgramme()
+        {
+            int arraylength;
+            Console.WriteLine("\nFind maximum and minimum element in an array");
+            Console.WriteLine("----------------------------------------");
+
+            Console.Write("Input the number of elements to be stored in the array :");
+            arraylength = Convert.ToInt32(Console.ReadLine());
+            int[] arr1 = new int[arraylength];
+
+            Console.Write("Input {0} elements in the array :\n", arraylength);
+            arr1 = Extensions.addNumberstoArray(arraylength);
+            int max = arr1.findMaxNumberinArray();
+            int min = arr1.findMinNumberinArray();
+
+            Console.Write("Maximum element is : {0}\n", max);
+            Console.Write("Minimum element is : {0}\n\n", min);
+        }
+
+        private static void roundProgramme()
+        {
+            Console.WriteLine("Enter the comma number you want to round(use ,)");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Number you entered : " + a);
+            var result = a.Round(1);
+            Console.WriteLine(a + " number is rounded to: " + result);
+
+        }
+
+
+    }
+}
